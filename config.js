9b2ce85984c6bad226597f85648dfe014e05937a
Ruben Quispe <ruben.q002@gmail.com>
2023-08-18 21:30:19 -0400

    build(api,public,store,views): Primer commit

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ module.exports = {
     host: process.env.SEQ_HOST || 'localhost',
     user: process.env.SEQ_USER || 'postgres',
     password: process.env.SEQ_PASS || 'post4u',
-    database: process.env.SEQ_DB || 'api_matriz',
+    database: process.env.SEQ_DB || 'matriz',
   },
 };
